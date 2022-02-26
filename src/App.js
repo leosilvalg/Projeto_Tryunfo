3f811f80ef7085be35327ef2bae897215e92fa4a
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-26 16:12:47 -0300

    Requisito 05

--- src/App.js
+++ src/App.js
@@ -16,9 +16,11 @@ class App extends React.Component {
       imageInput: '',
       trunfoInput: false,
       rareInput: '',
+      btnDisable: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
   handleChange({ target }) {
@@ -27,7 +29,38 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
-    });
+    }, () => this.validate());
+  }
+
+  // Pare realizar a validação tomei como base a aula extra do dia 25/02
+  validate() {
+    const { attr1Input, attr2Input, attr3Input } = this.state;
+    const {
+      nameInput,
+      descriptionInput,
+      imageInput,
+      rareInput,
+    } = this.state;
+    this.setState({ btnDisable: true });
+    const fields = [nameInput, descriptionInput, imageInput, rareInput];
+    const notEmpty = fields.every((field) => field !== '');
+
+    const maxValue = 210;
+    const maxIndValue = 90;
+    const zero = 0;
+    const attr1 = parseInt(attr1Input, 10);
+    const attr2 = parseInt(attr2Input, 10);
+    const attr3 = parseInt(attr3Input, 10);
+    const max = (attr1 + attr2 + attr3);
+
+    if (notEmpty
+      && (max <= maxValue)
+      && (attr1 <= maxIndValue && attr1 >= zero)
+      && (attr2 <= maxIndValue && attr2 >= zero)
+      && (attr3 <= maxIndValue && attr3 >= zero)
+    ) {
+      this.setState({ btnDisable: false });
+    }
   }
 
   render() {
@@ -40,6 +73,7 @@ class App extends React.Component {
       imageInput,
       trunfoInput,
       rareInput,
+      btnDisable,
     } = this.state;
 
     return (
@@ -55,7 +89,7 @@ class App extends React.Component {
           cardImage={ imageInput }
           cardTrunfo={ trunfoInput }
           onInputChange={ this.handleChange }
-
+          isSaveButtonDisabled={ btnDisable }
         />
         <Card
           cardName={ nameInput }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -105,7 +105,7 @@ class Form extends React.Component {
             />
           </label>
           <button
-            type="button"
+            type="submit"
             data-testid="save-button"
             disabled={ isSaveButtonDisabled }
             onClick={ onSaveButtonClick }
